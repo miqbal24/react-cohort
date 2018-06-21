ad767e3be8febe4b1e274f85f787971b0d082838
Maheen Iqbal <maheen.iqbal@gmail.com>
2018-06-20 23:35:40 -0700

    MI: cleanup

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import { render } from 'react-dom';
-import AppContainer from './containers/AppContainer';
-const data = {
-    days : {
+import ReactDOM from 'react-dom';
+import CohortGraph from 'react-cohort-graph';
+
+class MyCohortGraph extends React.Component {
+  render() {
+    const data = { Days : {
         "05-22-2016": [200, 180.87, 120, 80, 50, 40, 30, 20, 50, 40, 23, 43, 20, 34, 30, 20, 10, 5],
         "05-23-2016": [300, 180, 120, 80, 50, 20, 15, 10, 5, 5, 5, 40, 23, 15, 10, 5, 1],
         "05-24-2016": [200, 180, 120, 80, 50, 20, 15, 10, 5, 5, 5, 4, 2, 1, 4, 5],
@@ -22,8 +25,19 @@ const data = {
         "06-08-2016": [300, 180],
         "06-09-2016": [200]
     }
-};
-render(
-  <AppContainer data={data} dataType='123' />,
-  document.getElementById('root')
-);
+  };
+    return (      
+        <div>
+          <h1>Lifetime Value Cohort</h1>
+            <CohortGraph 
+              data={data}
+              //dataType={'123'}
+              defaultValueType={'value'}
+              shadeColor={'#ff69b4'}
+            />
+        </div>
+    );
+  }
+}
+  
+  ReactDOM.render(<MyCohortGraph />, document.getElementById('root'));
